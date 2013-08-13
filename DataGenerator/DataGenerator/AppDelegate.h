12af645db6c52b2dba98f1c6b16e4102afa425ff
cmdaltent <weissbach.martin@gmail.com>
2013-08-13 15:17:32 +0200

    Initial commit of Xcode project

---
+++ DataGenerator/DataGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DataGenerator
+//
+//  Created by Martin Weißbach on 8/13/13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DataGenerator/DataGenerator/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  DataGenerator
+//
+//  Created by Martin Weißbach on 8/13/13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
