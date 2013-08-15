4290b4977909231f874d4b0cc9574facc7b6c0d9
Markus Wutzler <markus.wutzler@gmail.com>
2013-08-15 20:47:29 +0200

    MFI-16 MFI-23 + ACDSense.msdl: Initial Service Description containing (de)registering of Publisher / Receiver and basic sensor value exchange + ACDSenseService: ServiceStub for MobilisServer + MSDL Transformations + dummy ant script, didn't test o updated .gitignore

---
+++ SensorService/src/de/tudresden/inf/mobilis/acdsense/sensorservice/ACDSenseService.java
@@ -0,0 +1,13 @@
+package de.tudresden.inf.mobilis.acdsense.sensorservice;
+
+import de.tudresden.inf.rn.mobilis.server.services.MobilisService;
+
+public class ACDSenseService extends MobilisService {
+
+	@Override
+	protected void registerPacketListener() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
