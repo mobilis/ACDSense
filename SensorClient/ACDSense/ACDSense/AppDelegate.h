8e37c0b2eb48cca6db25c47834a9228a27e6e2e4
cmdaltent <weissbach.martin@gmail.com>
2013-08-19 15:29:36 +0200

    MFI-9 Initial commit for iOS project

---
+++ SensorClient/ACDSense/ACDSense/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ACDSense
+//
+//  Created by Martin Weißbach on 8/19/13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
