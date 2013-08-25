1e0f4fa525d0aa71c611f7451e191d363b37f2a1
cmdaltent <weissbach.martin@gmail.com>
2013-08-25 22:30:37 +0200

    o IncomingBeanDetection – memory management enhancements

--- SensorClient/ACDSense/ACDSense/IncomingBeanDetection.m
+++ SensorClient/ACDSense/ACDSense/IncomingBeanDetection.m
@@ -20,7 +20,7 @@
     classes = NULL;
     numberOfClasses = objc_getClassList(NULL, 0);
     
-    NSMutableArray *incomingBeans = [NSMutableArray arrayWithCapacity:10];
+    NSMutableArray *incomingBeans = [[NSMutableArray alloc] initWithCapacity:10];
     if (numberOfClasses > 0) {
         classes = malloc(sizeof(Class) * numberOfClasses);
         numberOfClasses = objc_getClassList(classes, numberOfClasses);
@@ -28,13 +28,13 @@
             Class class = classes[i];
             if (class_getClassMethod(class, @selector(conformsToProtocol:))) {
                 if ([class conformsToProtocol:@protocol(MXiIncomingBean)]) {
-                    [incomingBeans addObject:class];
+                    [incomingBeans addObject:[[[class alloc] init] autorelease]];
                 }
             }
         }
         free(classes);
     }
-    return incomingBeans;
+    return [incomingBeans autorelease];
 }
 
 
