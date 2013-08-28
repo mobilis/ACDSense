84e15abf0367d681cae6ec9d3cc85821d6cc6807
mr.VVoo <markus.wutzler@tu-dresden.de>
2013-08-28 21:34:53 +0200

    MFI-25 added split screen in detail view (arrangement with auto layout)

---
+++ SensorClient/ACDSense/ACDSense/SensorsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SensorsViewController.h
+//  ACDSense
+//
+//  Created by Markus Wutzler on 28.08.13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *sensorsView;
+@property (weak, nonatomic) IBOutlet UIView *sensorDetailView;
+
+
+@end
---
+++ SensorClient/ACDSense/ACDSense/SensorsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SensorsViewController.m
+//  ACDSense
+//
+//  Created by Markus Wutzler on 28.08.13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import "SensorsViewController.h"
+
+@interface SensorsViewController ()
+
+@end
+
+@implementation SensorsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
