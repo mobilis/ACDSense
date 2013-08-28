8dfa6645c147d3457f808e74718e3a3c8efdcc10
mr.VVoo <markus.wutzler@tu-dresden.de>
2013-08-28 15:23:19 +0200

    MFI-25 minor fix in AppDelegate

--- SensorClient/ACDSense/ACDSense/AppDelegate.m
+++ SensorClient/ACDSense/ACDSense/AppDelegate.m
@@ -24,6 +24,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+	[self launchConnectionEstablishment];
     return YES;
 }
 
@@ -48,7 +49,6 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [self launchConnectionEstablishment];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
