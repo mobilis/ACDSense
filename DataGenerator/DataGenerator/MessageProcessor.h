9898c1b6b61ab3334cb88f5df832833fa6710bb4
cmdaltent <weissbach.martin@gmail.com>
2013-09-26 11:15:45 +0200

    o MessageProcessor 	– added method to use the current time as timestamp in the chat message 	– added documentation

--- DataGenerator/DataGenerator/MessageProcessor.h
+++ DataGenerator/DataGenerator/MessageProcessor.h
@@ -17,11 +17,21 @@
 *   {"sensorevent":{"type":"AMBIENT_TEMPERATURE","value":[20.34432],"timestamp":"2013-09-18T18:31:38+1:00"}}
 *
 *   @param sensorValue SensorValue to encode as multi-user-chat-message.
+*   @param timestampAsString Timestamp in a string representation to use in the message.
 *
 *   @return A json-String that conforms to the example above.
 */
 + (NSString *)messageWithSensorValue:(SensorValue *)sensorValue;
++ (NSString *)messageWithSEnsorValue:(SensorValue *)sensorValue andTimestamp:(NSString *)timestampAsString;
 
+/**
+ *  Creates a string representation of a sensor value with the given test value as sensed value.
+ *  {"sensorevent":{"type":"AMBIENT_TEMPERATURE","value":[20.34432],"timestamp":"2013-09-18T18:31:38+1:00"}}
+ *
+ *  @param testValue The testValue to encode in the json string.
+ *
+ *  @return A json-String that conforms to the example above.
+ */
 + (NSString *)messageWithTestValue:(NSUInteger)testValue;
 
 @end
--- DataGenerator/DataGenerator/MessageProcessor.m
+++ DataGenerator/DataGenerator/MessageProcessor.m
@@ -14,6 +14,31 @@
 
 + (NSString *)messageWithSensorValue:(SensorValue *)sensorValue
 {
+    return [self messageWithSEnsorValue:sensorValue andTimestamp:nil];
+}
+
++ (NSString *)messageWithSEnsorValue:(SensorValue *)sensorValue andTimestamp:(NSString *)timestampAsString
+{
+    NSString *timestamp = nil;
+    if (timestampAsString)
+        timestamp = timestampAsString;
+    else {
+        NSDate *date = [NSDate date];
+        unsigned long components = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
+        NSCalendar *currentCalendar = [NSCalendar currentCalendar];
+        NSDateComponents *dateComponents = [currentCalendar components:components fromDate:date];
+        
+        NSMutableString *timestampBuilder = [NSMutableString new];
+        [timestampBuilder appendFormat:@"%ld-", (long)dateComponents.year];
+        [timestampBuilder appendFormat:@"%02ld-", (long)dateComponents.month];
+        [timestampBuilder appendFormat:@"%02ldT", (long)dateComponents.day];
+        [timestampBuilder appendFormat:@"%02ld:", (long)dateComponents.hour];
+        [timestampBuilder appendFormat:@"%02ld:", (long)dateComponents.minute];
+        [timestampBuilder appendFormat:@"%02ld+1:00", (long)dateComponents.second];
+        
+        timestamp = [NSString stringWithString:timestampBuilder];
+    }
+    
     // {"sensorevent":{"type":"AMBIENT_TEMPERATURE","value":[20.34432],"timestamp":"2013-09-18T18:31:38+1:00"}}
     NSMutableString *jsonString = [NSMutableString stringWithCapacity:100];
     [jsonString appendString:@"{\"sensorevent\":{\"type\":\""];
@@ -21,9 +46,9 @@
     [jsonString appendString:@"\",\"values\":["];
     [jsonString appendString:sensorValue.value];
     [jsonString appendString:@"],\"timestamp\":\""];
-    [jsonString appendString:[sensorValue.timestamp timestampAsString]];
+    [jsonString appendString:timestamp];
     [jsonString appendString:@"\"}}"];
-
+    
     return jsonString;
 }
 
