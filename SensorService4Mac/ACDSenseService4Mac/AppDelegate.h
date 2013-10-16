7a78d829c9a231b442054daf111a2285f553747b
Martin Weißbach <martin@wc0343.dip.tu-dresden.de>
2013-10-16 18:08:12 +0200

    Initial commit

---
+++ SensorService4Mac/ACDSenseService4Mac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ACDSenseService4Mac
+//
+//  Created by Martin Weißbach on 10/16/13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SensorService4Mac/ACDSenseService4Mac/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  ACDSenseService4Mac
+//
+//  Created by Martin Weißbach on 10/16/13.
+//  Copyright (c) 2013 Technische Universität Dresden. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
